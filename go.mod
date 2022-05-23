81a7b653be4ac1a8532de5a93e655d1e4926c0aa
Karthikeyan Govindaraj <30545166+gkarthiks@users.noreply.github.com>
2022-05-23 20:32:20 +0000

    minor time pass refactor

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/gkarthiks/tamil-numerology-calc
 
 go 1.15
 
-require github.com/sirupsen/logrus v1.8.1 // indirect
+require github.com/sirupsen/logrus v1.8.1
--- main.go
+++ main.go
@@ -2,54 +2,25 @@ package main
 
 import (
 	"bufio"
-	"github.com/sirupsen/logrus"
 	"os"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 var (
-	alphaMap map[string]int
+	alphaMap      map[string]int
 	numerologySum = 0
 )
 
-func init() {
-	alphaMap = make(map[string]int)
-
-	alphaMap["a"]=1
-	alphaMap["b"]=2
-	alphaMap["c"]=3
-	alphaMap["d"]=4
-	alphaMap["e"]=5
-	alphaMap["f"]=8
-	alphaMap["g"]=3
-	alphaMap["h"]=5
-	alphaMap["i"]=1
-	alphaMap["j"]=1
-	alphaMap["k"]=2
-	alphaMap["l"]=3
-	alphaMap["m"]=4
-	alphaMap["n"]=5
-	alphaMap["o"]=7
-	alphaMap["p"]=8
-	alphaMap["q"]=1
-	alphaMap["r"]=2
-	alphaMap["s"]=3
-	alphaMap["t"]=4
-	alphaMap["u"]=6
-	alphaMap["v"]=6
-	alphaMap["w"]=6
-	alphaMap["x"]=5
-	alphaMap["y"]=1
-	alphaMap["z"]=7
-}
-
 func main() {
+	alphaMap = map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 8, "g": 3, "h": 5, "i": 1, "j": 1, "k": 2, "l": 3, "m": 4, "n": 5, "o": 7, "p": 8, "q": 1, "r": 2, "s": 3, "t": 4, "u": 6, "v": 6, "w": 6, "x": 5, "y": 1, "z": 7}
 	logrus.Println("Type the name and press enter ")
 	reader := bufio.NewReader(os.Stdin)
 	nameString, _ := reader.ReadString('\n')
 	nameString = strings.ToLower(nameString)
 
-	for i:=0; i< len(nameString); i++ {
+	for i := 0; i < len(nameString); i++ {
 		numerologySum = numerologySum + alphaMap[string(nameString[i])]
 	}
 
@@ -65,8 +36,8 @@ func main() {
 func getSimplifiedNum(nonSimplifiedNum int) int {
 	simplifiedNum := 0
 	for nonSimplifiedNum > 0 {
-		simplifiedNum = simplifiedNum + nonSimplifiedNum % 10
-		nonSimplifiedNum = nonSimplifiedNum/10
+		simplifiedNum = simplifiedNum + nonSimplifiedNum%10
+		nonSimplifiedNum = nonSimplifiedNum / 10
 	}
 	return simplifiedNum
 }
